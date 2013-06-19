7f222ead7a5d7d7e85f9a86c0aa38b8a4acf8353
Varun Varshney <no10downingstreet@gmail.com>
2013-06-19 17:16:21 +0530

    cleaned up unnecessary id assignments. renamed ids to be more readable. created VesproDataSource for handling of CRUD in TPCS_VESSEl created VesproModel for getting/setting data in TPCS_VESSEL added code for spinners, added code for editTexts

--- src/com/example/pcs/MySQLiteHelper.java
+++ src/com/example/pcs/MySQLiteHelper.java
@@ -11,8 +11,30 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 	public static final String DATABASE_NAME = "pcs";
 	public static final int DATABASE_VERSION = 1;
 
-	public static final String TPCS_VESSEL = "TPCS_VESSEl";
-
+	public static final String TABLE_TPCS_VESSEL = "TPCS_VESSEl";
+	
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_IMO_NUMBER = "imo_number";
+	public static final String COLUMN_VESSEL_NAME="vessel_name";
+	public static final String COLUMN_VESSEL_TYPE="vessel_type";
+	public static final String COLUMN_SR_CERTIFICATE_NO="sr_certificate_no";
+	public static final String COLUMN_AGENCY_CODE = "agency_code";
+	public static final String COLUMN_OWNER_NAME="owner_name";
+	public static final String COLUMN_OWNER_EMAIL="owner_email";
+	public static final String COLUMN_PORT_OF_SUBMISSION="port_of_submission";
+	public static final String COLUMN_NATIONALITY="nationality";
+	public static final String COLUMN_VESSEL_HEIGHT="vessel_height";
+	public static final String COLUMN_VESSEL_BREADTH="vessel_breadth";
+	public static final String COLUMN_VESSEL_LENGTH="vessel_length";
+	public static final String COLUMN_VESSEL_WEIGHT="vessel_weight";
+	public static final String COLUMN_INSURANCE_COMPANY="insurance_company";
+	public static final String COLUMN_INSURANCE_VALIDITY="insurance_validity";
+	public static final String COLUMN_PNI_CLUB = "pni_club";
+	public static final String COLUMN_PNI_INSURANCE_VALIDITY ="pni_insurance_validity";
+	public static final String COLUMN_VESSEL_GEARS="vessel_gears";
+	public static final String COLUMN_ENGINE_TYPE="engine_type";
+	public static final String COLUMN_NO_ENGINES="no_engines";
+		
 	public MySQLiteHelper(Context context, String name, CursorFactory factory,
 			int version) {
 		super(context, name, factory, version);
@@ -28,37 +50,38 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
 	public void onCreate(SQLiteDatabase database) {
 		// TODO Auto-generated method stub
 		database.execSQL("create table "
-				+ TPCS_VESSEL
-				+ "(_id int not null autoincrement, " +
-				"imo_number text primary key," +
-				" vessel_name text not null, " +
-				"vessel_type text not null, " +
-				"sr_certificate_no  text not null," +
-				" agency_code  text not null, " +
-				"owner_name  text not null," +
-				" owner_email  text not null, " +
-				"port_of_submission text not null, " +
-				"nationality  text not null, " +
-				"vessel_height text not null," +
-				" vessel_breadth text not null," +
-				"vessel_length text not null," +
-				" vessel_weight text not null," +
-				"insurance_company text not null," +
-				"insurance_validity text not null," +
-				"pni_club text not null," +
-				"pni_insurance_validity text not null," +
-				"vessel_gears text not null," +
-				"engine_type text not null," +
-				"no_engines int not null);");
+				+ TABLE_TPCS_VESSEL
+				+ "("+ COLUMN_ID + " integer primary key autoincrement, " +
+				       COLUMN_IMO_NUMBER +" text not null, " +
+				       COLUMN_VESSEL_NAME + " text not null, " +
+				       COLUMN_VESSEL_TYPE +  " text not null, " +
+				       COLUMN_SR_CERTIFICATE_NO +" text not null," +
+				       COLUMN_AGENCY_CODE + " text not null, " +
+				       COLUMN_OWNER_NAME + " text not null, " +
+				       COLUMN_OWNER_EMAIL + " text not null, " +
+				       COLUMN_PORT_OF_SUBMISSION + " text not null, " +
+				       COLUMN_NATIONALITY+ " text not null, " +
+				       COLUMN_VESSEL_HEIGHT + " text not null, " +
+				       COLUMN_VESSEL_BREADTH + " text not null, " +
+				       COLUMN_VESSEL_LENGTH + " text not null, " +
+				       COLUMN_VESSEL_WEIGHT + " text not null, " +
+				       COLUMN_INSURANCE_COMPANY + " text not null, " +
+				       COLUMN_INSURANCE_VALIDITY + " text not null, " +
+				       COLUMN_PNI_CLUB + " text not null, " +
+				       COLUMN_PNI_INSURANCE_VALIDITY + " text not null," +
+				       COLUMN_VESSEL_GEARS + " text not null, " +
+				       COLUMN_ENGINE_TYPE + " text not null, " +
+					   COLUMN_NO_ENGINES + " integer not null);");
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
 		Log.w(MySQLiteHelper.class.getName(),
-				"Upgrading database from version " + oldVersion + " to "
-						+ newVersion + ", which will destroy all old data");
-		db.execSQL("DROP TABLE IF EXISTS " + TPCS_VESSEL);
+			  "Upgrading database from version " + oldVersion + " to "
+			  + newVersion + ", which will destroy all old data");
+		
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_TPCS_VESSEL);
 		onCreate(db);
 
 	}
--- src/vessel/vespro.java
+++ src/vessel/vespro.java
@@ -3,38 +3,100 @@ package vessel;
 import com.example.pcs.R;
 import com.example.pcs.success;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-public class vespro extends Activity{
+public class vespro extends Activity {
 
-	Button b1,b2,b3,b4,submit,save,cancel;
-	EditText vessel,imo,reg_no,agency_code,owner,o_email,weight,ins_comp,ins_date,pi_name,pi_date,gear_no,engine;
-	Spinner vessel_type,sub_port,nation;
+	Button submit, save, cancel;
+	EditText vessel_name, imo_number, sr_certificate_no, agency_code,
+			owner_name, owner_email, nationality, vessel_weight,vessel_length,vessel_breadth,vessel_height,
+			insurance_comp_name, ins_validity_date, pi_name, pi_date,
+			vessel_gears, engine_type, no_engines;
+	Spinner vessel_type, sub_port;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView (R.layout.vespro);
+		setContentView(R.layout.vespro);
 		
-		submit=(Button) findViewById (R.id.button1);
-		submit.setOnClickListener(new OnClickListener(){
+		vessel_name = (EditText) findViewById(R.id.vessel_name);
+		imo_number = (EditText) findViewById(R.id.imo_number);
+		sr_certificate_no = (EditText) findViewById(R.id.sr_certificate_no);
+		agency_code = (EditText) findViewById(R.id.agency_code);
+		owner_name = (EditText) findViewById(R.id.owner_name);
+		owner_email = (EditText) findViewById(R.id.owner_email);
+		nationality = (EditText) findViewById(R.id.nationality);
+		vessel_weight = (EditText) findViewById(R.id.vessel_weight);
+		vessel_length = (EditText) findViewById(R.id.vessel_length);
+		vessel_breadth = (EditText) findViewById(R.id.vessel_breadth);
+		vessel_height = (EditText) findViewById(R.id.vessel_height);
+		insurance_comp_name = (EditText) findViewById(R.id.insurance_comp_name);
+		ins_validity_date = (EditText) findViewById(R.id.insurance_validity_date);
+		pi_name = (EditText) findViewById(R.id.pni_club_name);
+		pi_date = (EditText) findViewById(R.id.pni_insur_val);
+		vessel_gears = (EditText) findViewById(R.id.vessel_gears);
+		engine_type = (EditText) findViewById(R.id.engine_type);
+		no_engines = (EditText) findViewById(R.id.no_engines);
 
+		vessel_type = (Spinner) findViewById(R.id.spinner_type_of_vessel);
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.type_of_vessel_array,
+				android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		vessel_type.setAdapter(adapter);
+		vessel_type
+				.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+					public void onItemSelected(AdapterView<?> parent,
+							View view, int pos, long id) {
+						Object item = parent.getItemAtPosition(pos);
+					}
+
+					public void onNothingSelected(AdapterView<?> parent) {
+					}
+				});
+
+		sub_port = (Spinner) findViewById(R.id.spinner_submission_port);
+		ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(
+				this, R.array.submission_port_array,
+				android.R.layout.simple_spinner_item);
+		adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		sub_port.setAdapter(adapter2);
+		sub_port.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int pos, long id) {
+				Object item2 = parent.getItemAtPosition(pos);
+			}
+
+			public void onNothingSelected(AdapterView<?> parent) {
+			}
+		});
+
+		submit = (Button) findViewById(R.id.button1);
+		submit.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent success = new Intent (vespro.this, success.class );
+				Intent success = new Intent(vespro.this, success.class);
 				startActivity(success);
-				
 			}
-			
 		});
-	}	
+		
+		
+		
+		
+		
+		
+		
+	}
 }
